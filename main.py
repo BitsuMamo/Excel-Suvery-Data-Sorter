3e1f83df14c75a2ed4dc352aa0e4cee73fca720d
Bitsu A. Mamo <54959510+BitsuAMamo@users.noreply.github.com>
2021-10-04 17:08:40 +0300

    Deleted main.py and excel_sorter.bat since all of that is used in the UI.

--- main.py
+++
@@ -1,6 +0,0 @@
-from SortData import SortData
-
-if __name__ == "__main__":
-    sort = SortData(input("Enter Path: "), input("Column of Num: "), input("Column X: "),input("Column Y: "),input("Column Elevation: "), input("Column Type: "), input("Column For X, Y, ELE Concat: "), input("Column For All Concat: "), float(input("X Bound: ")), float(input("Y Bound: ")))
-    #sort = SortData("example.xlsx", "A", "B",300000,"C",700000,"D","E","F","G")
-    sort.run()
